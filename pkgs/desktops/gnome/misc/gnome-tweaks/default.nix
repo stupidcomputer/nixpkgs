8a7201aa24fa81afd5310551ba2e361980d2c6b5
Maxine Aubrey <max@ine.dev>
2024-03-28 09:20:59 +0100

    fixup! gnome.gnome-tweaks: 45.1 → 46.0
    drop included patches

--- pkgs/desktops/gnome/misc/gnome-tweaks/default.nix
+++ pkgs/desktops/gnome/misc/gnome-tweaks/default.nix
@@ -2,7 +2,6 @@
 , meson
 , ninja
 , fetchurl
-, fetchpatch2
 , desktop-file-utils
 , gdk-pixbuf
 , gettext
@@ -31,21 +30,6 @@ python3Packages.buildPythonApplication rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
     sha256 = "sha256-Fnh4Y0H2ZKxFgHhCIqFkCfqb9cx6XxtG3O/SqhPdujE=";
   };
-
-  patches = [
-    # Avoid using Gtk.PropertyExpression (compatibility with pygobject < 3.47.0)
-    # https://gitlab.gnome.org/GNOME/gnome-tweaks/-/merge_requests/133
-    (fetchpatch2 {
-      url = "https://gitlab.gnome.org/GNOME/gnome-tweaks/-/commit/380909bac09131a847a6f8777a9e31c843ea4a67.patch";
-      hash = "sha256-aFFGL6ZDyhLZ51wdnE1XtHJQ+AB6ePS0mdlRHsUnaqI=";
-    })
-    # meson: Use gnome.post_install
-    # https://gitlab.gnome.org/GNOME/gnome-tweaks/-/merge_requests/131
-    (fetchpatch2 {
-      url = "https://gitlab.gnome.org/GNOME/gnome-tweaks/-/commit/eec610429bcf04fbde31ecb214b4e5d4508654d2.patch";
-      hash = "sha256-h4fvRH8RSj2LelwuKCyNIIeDX8/QQBBR0PUPam3ut+0=";
-    })
-  ];
 
   nativeBuildInputs = [
     desktop-file-utils
